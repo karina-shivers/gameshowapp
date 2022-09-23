5c36eba53b2ffe90e1c180eee4cfb2dce7f262b6
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-09-22 19:26:46 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+let newGame;
+const startButton = document.getElementById('btn__reset');
+startButton.addEventListener('click',()=> {
+    const name = new Game();
+    newGame = name;
+    newGame.startGame();
+});
+// listens for keyboard input
+for (key of document.getElementById('qwerty').querySelectorAll('.key')) {
+    key.addEventListener('click', (e) => {
+        newGame.handleInteraction(e.target);
+        console.log(newGame.missed);
+    })
+}
+// listens for phys.keyboard
+document.addEventListener('keydown', (e) => {
+    console.log(e); 
+    if (document.getElementById('overlay').style.display === 'none') {
+        for (const screenKey of document.getElementById('qwerty').querySelectorAll('.key')) {
+            if (screenKey.innerText.includes(e.key)) {
+                newGame.handleInteraction(screenKey);
+            }
+       }   }
+})

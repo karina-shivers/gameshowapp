2df7614626bae0d192a0237fa00bce42c8f2e014
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-09-22 19:30:39 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase){
+        this.phrase  = phrase.toLowerCase();
+    }
+}
+
+addPhraseToDisplay () {
+    document.querySelector('#phrase ul').innerHTML  = '';
+    document.querySelector('#phrase ul').insertAdjacentHTML("beforeend",
+        Array.from(this.phrase).map(letter => {
+            if(letter === ' ') {
+                return `<li class="space"> </li>`
+            } else {
+                return `<li class="hide letter ${letter}">${letter}</li>`
+            }
+        }).join('')
+    ) 
+}
+checkLetter(letterPress) {
+    return (this.phrase.includes(letterPress.innerText || letterPress.key))
+}
+showMatchedLetter(letter) {
+    Array.from(document.getElementsByClassName(letter)).map(element => {
+        element.classList.add("show"); 
+        element.classList.remove("hide");
+    })
+}

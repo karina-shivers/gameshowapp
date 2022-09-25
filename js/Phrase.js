4e69793201f7d77132733e9a75670420c8e67916
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-09-24 20:02:35 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,29 +2,45 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 class Phrase {
-    constructor(phrase){
-        this.phrase  = phrase.toLowerCase();
-    }
-}
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
 
-addPhraseToDisplay () {
-    document.querySelector('#phrase ul').innerHTML  = '';
-    document.querySelector('#phrase ul').insertAdjacentHTML("beforeend",
-        Array.from(this.phrase).map(letter => {
-            if(letter === ' ') {
-                return `<li class="space"> </li>`
-            } else {
-                return `<li class="hide letter ${letter}">${letter}</li>`
-            }
-        }).join('')
-    ) 
-}
-checkLetter(letterPress) {
-    return (this.phrase.includes(letterPress.innerText || letterPress.key))
-}
-showMatchedLetter(letter) {
-    Array.from(document.getElementsByClassName(letter)).map(element => {
-        element.classList.add("show"); 
-        element.classList.remove("hide");
-    })
+  // this method loops over the letters in the phrase and adds each letter to the display when the game starts.
+  addPhraseToDisplay() {
+    const phraseDiv = document.querySelector("#phrase ul");
+    const phraseArray = this.phrase.split("");
+    phraseArray.forEach((letter) => {
+      const li = document.createElement("li");
+      li.textContent = letter;
+      if (letter !== " ") {
+        li.className = `hide letter ${letter}`;
+      } else {
+        li.className = "space";
+      }
+      phraseDiv.appendChild(li);
+    });
+  }
+
+  // this method checks to see if the letter selected by the player matches a letter in the phrase.
+  checkLetter(letter) {
+    const phraseArray = this.phrase.split("");
+    if (phraseArray.includes(letter)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // this method checks to see if the player has revealed all of the letters in the active phrase.
+  showMatchedLetter(letter) {
+    const selectedPhrase = document.getElementById("phrase");
+    const childOfPhrase = selectedPhrase.firstElementChild;
+    const liLetters = childOfPhrase.getElementsByTagName("li");
+    for (let i = 0; i < liLetters.length; i++) {
+      if (liLetters[i].textContent === letter) {
+        liLetters[i].classList.replace("hide", "show");
+      }
+    }
+  }
 }

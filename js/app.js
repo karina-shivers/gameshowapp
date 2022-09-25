7fc6b636c67d776f066aaf6ac38b66e2fe35bf15
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-09-24 20:03:58 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-let newGame;
-const startButton = document.getElementById('btn__reset');
-startButton.addEventListener('click',()=> {
-    const name = new Game();
-    newGame = name;
-    newGame.startGame();
+// Event listener for the start game button
+let game;
+const startNewGame = document.querySelector("#btn__reset");
+startNewGame.addEventListener("click", (e) => {
+  game = new Game();
+  game.resetGame(e);
+  game.startGame();
 });
-// listens for keyboard input
-for (key of document.getElementById('qwerty').querySelectorAll('.key')) {
-    key.addEventListener('click', (e) => {
-        newGame.handleInteraction(e.target);
-        console.log(newGame.missed);
-    })
-}
-// listens for phys.keyboard
-document.addEventListener('keydown', (e) => {
-    console.log(e); 
-    if (document.getElementById('overlay').style.display === 'none') {
-        for (const screenKey of document.getElementById('qwerty').querySelectorAll('.key')) {
-            if (screenKey.innerText.includes(e.key)) {
-                newGame.handleInteraction(screenKey);
-            }
-       }   }
-})
+
+// Event listener for the onscreen keyboard to call the handleInteraction() method
+const button = document.querySelector("#qwerty");
+button.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    game.handleInteraction(e.target);
+  }
+});
+
+// Event listener for the physical keyboard to call the handleInteraction() method
+document.addEventListener("keydown", (event) => {
+  let keyPress = event.key;
+  const LETTERS = document.getElementsByClassName("key");
+  if (/\w/.test(keyPress)) {
+    for (let i = 0; i < LETTERS.length; i++) {
+      if (
+        keyPress === LETTERS[i].textContent &&
+        LETTERS[i].disabled === false
+      ) {
+        game.handleInteraction(LETTERS[i]);
+      }
+    }
+  }
+});

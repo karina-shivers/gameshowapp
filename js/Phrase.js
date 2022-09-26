ec6c39fcdf0518f4f9bdf958f07cf5c90d946385
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-09-26 13:14:40 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,44 +1,42 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+const phraseDiv = document.getElementById('phrase')
+
+
 class Phrase {
-  constructor(phrase) {
-    this.phrase = phrase.toLowerCase();
+  constructor (phrase) {
+    this.phrase = phrase.toLowerCase()
   }
 
-  // loops over the letters
-    const phraseDiv = document.querySelector("#phrase ul");
-    const phraseArray = this.phrase.split("");
-    phraseArray.forEach((letter) => {
-      const li = document.createElement("li");
-      li.textContent = letter;
-      if (letter !== " ") {
-        li.className = `hide letter ${letter}`;
+  addPhraseToDisplay () {
+    const splitPhrase = this.phrase.split('')
+    const phraseSection = document.getElementById('phraseSection')
+    for (let i = 0; i < splitPhrase.length; i++) {
+      if (splitPhrase[i] === ' ') {
+        phraseSection.innerHTML += '<li class="space"> </li>'
       } else {
-        li.className = "space";
+        phraseSection.innerHTML += `<li class="hide letter ${splitPhrase[i]}">${splitPhrase[i]}</li>`
       }
-      phraseDiv.appendChild(li);
-    });
-  }
-
-  //  checks to see if the letter selected by the player 
-  checkLetter(letter) {
-    const phraseArray = this.phrase.split("");
-    if (phraseArray.includes(letter)) {
-      return true;
-    } else {
-      return false;
     }
   }
 
-  //  checks to see if the player has revealed all of the letters 
-  showMatchedLetter(letter) {
-    const selectedPhrase = document.getElementById("phrase");
-    const childOfPhrase = selectedPhrase.firstElementChild;
-    const liLetters = childOfPhrase.getElementsByTagName("li");
-    for (let i = 0; i < liLetters.length; i++) {
-      if (liLetters[i].textContent === letter) {
-        liLetters[i].classList.replace("hide", "show");
+  
+  checkLetter (letter) {
+    if (this.phrase.includes(letter)) {
+      this.showMatchedLetter(letter)
+      return true
+    } else {
+      return false
+    }
+  }
+
+ 
+  showMatchedLetter (letter) {
+    const lettersInPhrase = ul.children
+    for (let i = 0; i < lettersInPhrase.length; i++) {
+      if (letter === lettersInPhrase[i].textContent) {
+        lettersInPhrase[i].className = `show letter ${letter}`
       }
     }
   }

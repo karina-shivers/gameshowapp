92fb9f99147c99baa459f53ba3d815d8ad2f242a
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-09-27 15:37:14 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,52 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-const game = new Game()
-const startButton = document.getElementById('btn__reset')
-startButton.addEventListener('click', (e) => {
-  game.resetGame(e)
-  game.startGame()
-})
+let game;
+const mainButton = document.querySelector('#btn__reset');
+const phrase = document.querySelector('#phrase').firstElementChild;
+const letters = document.getElementsByClassName("key");
+const hearts = document.querySelector('#scoreboard').firstElementChild.children;
 
-const keyboard = document.getElementById('qwerty')
-const keys = document.querySelectorAll('.key')
+/**
+ * Starts game and also resets the app after a gameover
+ */
+mainButton.addEventListener('click', () => {
+    game = new Game();
+    phrase.replaceChildren('');
 
-keyboard.onclick = (e) => {
-  const target = e.target
-  if (target.className === 'key') {
-    game.handleInteraction(target)
-  }
+    for (const letter of letters) {
+        letter.disabled = false;
+        letter.className = 'key';
+    }
+
+    for (const heart of hearts) {
+        heart.innerHTML = `<li class="tries"><img src="images/liveHeart.png" alt="Heart Icon" height="35" width="30"></li>`;
+    }
+
+    game.startGame();
+});
+
+/**
+ * blurs after click because keydown event does not trigger
+ * after a letter is disabled from a click
+ */
+for (const letter of letters) {
+    letter.addEventListener('click', (e) => {
+        game.handleInteraction(e.target);
+        document.activeElement.blur();
+    });
 }
 
-document.addEventListener('keyup', (e) => {
-  for (let i = 0; i < keys.length; i++) {
-    if (e.key === keys[i].innerHTML && keys[i].disabled === false) {
-      game.handleInteraction(keys[i])
+/**
+ * matches key with button if it not already disabled
+ * checking for enabled/disabled prevents key from being spammed and running out of lives
+ */
+document.addEventListener('keydown', (e) => {
+    for (const letter of letters) {
+        if (!letter.disabled) {
+            if (letter.innerText === e.key) {
+                game.handleInteraction(letter);
+            }
+        }
     }
-  }
-})
+});
